a568bdc046bc939b03912155c374734f841ec9b7
pop sukanya <65657379+popkiki@users.noreply.github.com>
2020-06-22 11:35:34 +0700

    ทำการแก้admin
    ลบdocument.php โค้ดเกิดการบัก แต่สามารถใส่หน้าลิงแอดมินลงadminpage.phpได้แล้ว

---
+++ adminlink.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index page</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+</head>
+<body>
+            <div class="wrapper">
+                <div class="sidebar">
+                    <h2>admin</h2>
+                    <ul>
+                        <li><a href="#"><i class="fas fa-home"></i>หน้าหลัก</a></li>
+                        <li><a href="#"><i class="fas fa-home"></i>จัดการข้อมูลชุมชน</a></li>
+                        <li><a href="#"><i class="fas fa-user"></i>จัดการข้อมูลศิลปวัฒนธรรม</a></li>
+                        <li><a href="#"><i class="fas fa-address-card"></i>จัดการข้อมูลสินค้าโอทอป</a></li>
+                        <li><a href="#"><i class="fas fa-project-diagram"></i>จัดการข้อมูลสินค้าน่าสนใจ</a></li>
+                        <li><a href="#"><i class="fas fa-blog"></i>จัดการข้อมูลกลุ่มแม่บ้าน</a></li>
+                        <li><a href="#"><i class="fas fa-address-book"></i>สรุปยอดขาย</a></li>
+                        <li><a href="#"><i class="fas fa-map-pin"></i>ออกจากระบบ</a></li>
+                    </ul> 
+</body>
---
+++ adminpage.php
@@ -0,0 +1,23 @@
+<?
+require_once "header.php"
+?>
+  
+        
+            <!--*********************end content-second ********************************--> 
+             
+            <div class="wrapper">
+                <div class="sidebar">
+                    <h2>admin</h2>
+                    <ul>
+                        <li><a href="#"><i class="fas fa-home"></i>หน้าหลัก</a></li>
+                        <li><a href="#"><i class="fas fa-home"></i>จัดการข้อมูลชุมชน</a></li>
+                        <li><a href="#"><i class="fas fa-user"></i>จัดการข้อมูลศิลปวัฒนธรรม</a></li>
+                        <li><a href="#"><i class="fas fa-address-card"></i>จัดการข้อมูลสินค้าโอทอป</a></li>
+                        <li><a href="#"><i class="fas fa-project-diagram"></i>จัดการข้อมูลสินค้าน่าสนใจ</a></li>
+                        <li><a href="#"><i class="fas fa-blog"></i>จัดการข้อมูลกลุ่มแม่บ้าน</a></li>
+                        <li><a href="#"><i class="fas fa-address-book"></i>สรุปยอดขาย</a></li>
+                        <li><a href="#"><i class="fas fa-map-pin"></i>ออกจากระบบ</a></li>
+                    </ul> 
+<?
+require_once "footer.php"
+?>
