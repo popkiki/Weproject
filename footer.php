4ea5012f0eb11dfde6c9e20afceca2649e1c1ac0
pop sukanya <65657379+popkiki@users.noreply.github.com>
2020-08-06 15:59:55 +0700

    6/8/2020 ลิ้งเว็บ หน้าข้อมูลชุมชน
    เพิ่มลิงค์หน้าเว็บแต่ล่ะลิ้ง เขียนตัวheaderกับfooterตัวเต็ม  เพิ่มหน้าdocument

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
 <div class="containrt">
     <div class="footer">
         <ul >
-            <li><a href="index.php">หน้าหลัก</a></li>
-            <li><a href="login.php">สมาชิก</a></li>
-            <li><a href="document.php">บทความ</a></li>
-            <li><a href="store.php">สินค้าโอทอป</a></li>
-            <li><a href="packagenumber.php">แจ้งเลขพัสดุ</a></li>
-            <li><a href="payment.php">แจ้งชำระเงิน</a></li>
-            <li><a href="contact.php">ติดต่อเรา</a></li>
+            <li><a href="index.html">หน้าหลัก</a></li>
+            <li><a href="login.html">สมาชิก</a></li>
+            <li><a href="document.html">บทความ</a></li>
+            <li><a href="store.html">สินค้าโอทอป</a></li>
+            <li><a href="packagenumber.html">แจ้งเลขพัสดุ</a></li>
+            <li><a href="payment.html">แจ้งชำระเงิน</a></li>
+            <li><a href="contact.html">ติดต่อเรา</a></li>
             </ul>
 
             <div class="copyright">
